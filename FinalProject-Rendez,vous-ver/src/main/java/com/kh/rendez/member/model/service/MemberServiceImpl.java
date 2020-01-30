6dde7e36e8c4bdc6570717f2ba1efe43173f8cf9
sprin <sprin@DESKTOP-KC6V50V>
2020-01-30 12:53:07 +0900

    2020.01.30

--- FinalProject-Rendez,vous-ver/src/main/java/com/kh/rendez/member/model/service/MemberServiceImpl.java
+++ FinalProject-Rendez,vous-ver/src/main/java/com/kh/rendez/member/model/service/MemberServiceImpl.java
@@ -43,9 +43,11 @@ public class MemberServiceImpl implements MemberService {
 	}
 
 	@Override
-	public ArrayList<Qna> selectMyQnaList(int currentPage, int writer) {
+	public ArrayList<Qna> selectMyQnaList(int currentPage, String writer) {
 		int listCount = mDao.getMyQnaListCount(currentPage, writer);
+		System.out.println(listCount);
 		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
+		
 		return mDao.selectMyQnaList(pi, writer);
 	}
 
